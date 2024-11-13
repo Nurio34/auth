f45dfe8b80141109d7c0c385831c4e70e1afffba
Nurio34 <nurioonsoftware@gmail.com>
2024-11-13 21:28:31 +0300

    Fix_Vercel

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,34 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
-    withCredentials: true, // Ensures cookies are sent automatically
+    withCredentials: true,
 });
-
-// Helper function to get cookie value by name (client-side only)
-const getCookie = (name: string) => {
-    if (typeof window === "undefined") return null; // Avoid using document.cookie on the server
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(";").shift();
-    return null;
-};
-
-axiosInstance.interceptors.request.use(
-    (config) => {
-        // Get token from cookies (only in the browser)
-        const token = getCookie("token");
-
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        } else {
-            console.log("No token found in cookies");
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    },
-);
 
 export default axiosInstance;
